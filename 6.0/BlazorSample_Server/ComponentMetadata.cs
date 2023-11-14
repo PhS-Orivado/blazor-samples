0fac3c7f5218403bd41ed73cf36442f785337750
PhS-Orivado <89846465+PhS-Orivado@users.noreply.github.com>
2023-11-14 10:22:49 +0100

    Delete 6.0 directory

--- 6.0/BlazorSample_Server/ComponentMetadata.cs
+++
@@ -1,6 +0,0 @@
-﻿public class ComponentMetadata
-{
-    public string? Name { get; set; }
-    public Dictionary<string, object> Parameters { get; set; } =
-        new Dictionary<string, object>();
-}
